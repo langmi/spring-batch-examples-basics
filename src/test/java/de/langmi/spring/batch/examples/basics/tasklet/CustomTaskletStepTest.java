ecf26b7e4b2b25805710fa2cb93e59620499bc4d
Michael Pralow <me@michael-pralow.de>
2014-02-09 22:34:51 +0100

    refactored test package for same naming as main package

--- src/test/java/de/langmi/spring/batch/examples/basics/tasklet/CustomTaskletStepTest.java
+++ src/test/java/de/langmi/spring/batch/examples/basics/tasklet/CustomTaskletStepTest.java
@@ -1,55 +1,53 @@
-/**
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package de.langmi.spring.batch.examples.basics.taskletstep;
+package de.langmi.spring.batch.examples.basics.tasklet;
 
-import de.langmi.spring.batch.examples.basics.tasklet.SimpleTasklet;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
 /**
  * Simple test for {@link SimpleTaskletStep}.
  *
- * @author Michael R. Lange <michael.r.lange@langmi.de>
+ * @author Michael R. Pralow <me@michael-pralow.de>
  * @see <a
  * href="http://stackoverflow.com/questions/1119385/junit-test-for-system-out-println">JUnit
  * test for system.out.println</a>
  */
-public class SimpleTaskletStepTest {
+public class CustomTaskletStepTest {
 
     /**
      * Stream for catching System.out.
      */
     private final ByteArrayOutputStream newSysOut = new ByteArrayOutputStream();
     private PrintStream oldSysOut;
-    private final Tasklet tasklet = new SimpleTasklet();
+    private CustomTasklet tasklet = new CustomTasklet();
 
     @Test
     public void testExecute() throws Exception {
         // run the taskletStep, no need for contexts here
-        assertEquals(RepeatStatus.FINISHED, tasklet.execute(null, null));
+        assertEquals(RepeatStatus.FINISHED, tasklet.customMethod());
         // assert sysoutput
         assertEquals("Hello World!", newSysOut.toString());
     }
 
     @Before
-    public void setup() {
+    public void setUp() {
         // catch and set new system out
         oldSysOut = System.out;
         System.setOut(new PrintStream(newSysOut));
--- src/test/java/de/langmi/spring/batch/examples/basics/tasklet/SimpleTaskletStepTest.java
+++ src/test/java/de/langmi/spring/batch/examples/basics/tasklet/SimpleTaskletStepTest.java
@@ -1,54 +1,54 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
-package de.langmi.spring.batch.examples.basics.taskletstep;
+package de.langmi.spring.batch.examples.basics.tasklet;
 
-import de.langmi.spring.batch.examples.basics.tasklet.CustomTasklet;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
 /**
  * Simple test for {@link SimpleTaskletStep}.
  *
- * @author Michael R. Pralow <me@michael-pralow.de>
+ * @author Michael R. Lange <michael.r.lange@langmi.de>
  * @see <a
  * href="http://stackoverflow.com/questions/1119385/junit-test-for-system-out-println">JUnit
  * test for system.out.println</a>
  */
-public class CustomTaskletStepTest {
+public class SimpleTaskletStepTest {
 
     /**
      * Stream for catching System.out.
      */
     private final ByteArrayOutputStream newSysOut = new ByteArrayOutputStream();
     private PrintStream oldSysOut;
-    private CustomTasklet tasklet = new CustomTasklet();
+    private final Tasklet tasklet = new SimpleTasklet();
 
     @Test
     public void testExecute() throws Exception {
         // run the taskletStep, no need for contexts here
-        assertEquals(RepeatStatus.FINISHED, tasklet.customMethod());
+        assertEquals(RepeatStatus.FINISHED, tasklet.execute(null, null));
         // assert sysoutput
         assertEquals("Hello World!", newSysOut.toString());
     }
 
     @Before
-    public void setUp() {
+    public void setup() {
         // catch and set new system out
         oldSysOut = System.out;
         System.setOut(new PrintStream(newSysOut));
